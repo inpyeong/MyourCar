f7de79bef178654ee4f41bce89eb915332bbef75
inpyeong <jinipyung@gmail.com>
2020-10-12 16:29:20 +0900

    Add UserResponseDto

--- backend_springboot/src/main/java/com/MyourCar/web/dto/UserResponseDto.java
+++ backend_springboot/src/main/java/com/MyourCar/web/dto/UserResponseDto.java
@@ -1,2 +1,13 @@
-package com.MyourCar.web.dto;public class UserResponseDto {
+package com.MyourCar.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserResponseDto {
+
+    private final String name;
+    private final int amount;
+
 }
