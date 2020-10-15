2204ef93314dedf0eb802f6d6bbf4803e4bbe7ad
inpyeong <jinipyung@gmail.com>
2020-10-15 18:22:45 +0900

    Implement OAuth2 of Google

--- backend_springboot/src/main/java/com/MyourCar/config/auth/SecurityConfig.java
+++ backend_springboot/src/main/java/com/MyourCar/config/auth/SecurityConfig.java
@@ -1,2 +1,33 @@
-package com.MyourCar.config.auth;public class SecurityConfig {
+package com.MyourCar.config.auth;
+
+import com.MyourCar.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        // Deep Linking 해서 설정한 URL 작성하기.
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
 }
