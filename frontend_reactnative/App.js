0410358cf17adb2d24b5176feef2deef8e3bd7b3
inpyeong <jinipyung@gmail.com>
2020-10-15 14:18:29 +0900

    Write test code

--- frontend_reactnative/App.js
+++ frontend_reactnative/App.js
@@ -6,109 +6,187 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    StyleSheet,
+    View,
+    Text,
+    Button,
+    Image,
+    SafeAreaView,
+    StatusBar,
+    TouchableOpacity,
 } from 'react-native';
-
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+} from '@react-native-community/google-signin';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+        }
+    }
+
+    _signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            this.setState({ userInfo, loggedIn: true });
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+                console.log(1);
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (e.g. sign in) is in progress already
+                console.log(2);
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                // play services not available or outdated
+                console.log(3);
+            } else {
+                // some other error happened
+                console.log(4);
+                console.log(error);
+            }
+        }
+    }
+
+    signOut = async () => {
+        try {
+            await GoogleSignin.revokeAccess();
+            await GoogleSignin.signOut();
+            this.setState({ user: null, loggedIn: false }); // Remember to remove the user from your app's state as well
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    getCurrentUserInfo = async () => {
+        try {
+            const userInfo = await GoogleSignin.signInSilently();
+            this.setState({ userInfo }, () => {
+                console.log(this.state.userInfo);
+            });
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+                // user has not signed in yet
+            } else {
+                // some other error
+            }
+        }
+    }
+
+    getCurrentUser = async () => {
+        const currentUser = await GoogleSignin.getCurrentUser();
+        this.setState({ currentUser });
+    };
+
+    isSignedIn = async () => {
+        const isSignedIn = await GoogleSignin.isSignedIn();
+        this.setState({ isLoginScreenPresented: !isSignedIn });
+    };
+
+    componentDidMount = () => {
+        GoogleSignin.configure({
+            // webClientId: '218580306048-ikle5jtm4dpp21dilc9jedgiu7so4qvm.apps.googleusercontent.com',
+            // androidClientId: '318580306048-ikle5jtm4dpp21dilc9jedgiu7so4qvm.apps.googleusercontent.com',
+            // offlineAccess: true,
+        });
+    }
+
+    render() {
+        return (
+            <View>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView>
+                    <View style={styles.sectionContainer}>
+                        <GoogleSigninButton
+                            style={{ width: 192, height: 48 }}
+                            size={GoogleSigninButton.Size.Wide}
+                            color={GoogleSigninButton.Color.Dark}
+                            onPress={this._signIn}
+                            disabled={this.state.isSigninInProgress} />
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        {!this.state.loggedIn && <Text>You are currently logged out</Text>}
+                        {this.state.loggedIn && <Button onPress={this.signOut}
+                                                        title="Signout"
+                                                        color="#841584">
+                        </Button>}
+                    </View>
+                    {this.state.loggedIn && <View>
+                        <View style={styles.listHeader}>
+                            <Text>User Info</Text>
+                        </View>
+                        <View style={styles.dp}>
+                            <Image
+                                style={{ width: 100, height: 100 }}
+                                source={{ uri: this.state.userInfo && this.state.userInfo.user && this.state.userInfo.user.photo }}
+                            />
+                        </View>
+                        <View style={styles.detailContainer}>
+                            <Text style={styles.title}>Name</Text>
+                            <Text style={styles.message}>{this.state.userInfo && this.state.userInfo.user && this.state.userInfo.user.name}</Text>
+                        </View>
+                        <View style={styles.detailContainer}>
+                            <Text style={styles.title}>Email</Text>
+                            <Text style={styles.message}>{this.state.userInfo && this.state.userInfo.user && this.state.userInfo.user.email}</Text>
+                        </View>
+                        <View style={styles.detailContainer}>
+                            <Text style={styles.title}>ID</Text>
+                            <Text style={styles.message}>{this.state.userInfo && this.state.userInfo.user && this.state.userInfo.user.id}</Text>
+                        </View>
+                    </View>}
+                </SafeAreaView>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        );
+    }
+}
+
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    listHeader: {
+        backgroundColor: '#eee',
+        color: "#222",
+        height: 44,
+        padding: 12
+    },
+    detailContainer: {
+        paddingHorizontal: 20
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        paddingTop: 10
+    },
+    message: {
+        fontSize: 14,
+        paddingBottom: 15,
+        borderBottomColor: "#ccc",
+        borderBottomWidth: 1
+    },
+    dp:{
+        marginTop: 32,
+        paddingHorizontal: 24,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    }
 });
 
 export default App;
