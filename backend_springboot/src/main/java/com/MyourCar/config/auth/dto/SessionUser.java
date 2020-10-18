51ad8c3e61631af64ebb1d6c2e08f9aeccaed3e4
inpyeong <jinipyung@gmail.com>
2020-10-18 22:44:34 +0900

    Add members in user to SessionUser class

--- backend_springboot/src/main/java/com/MyourCar/config/auth/dto/SessionUser.java
+++ backend_springboot/src/main/java/com/MyourCar/config/auth/dto/SessionUser.java
@@ -8,10 +8,18 @@ import java.io.Serializable;
 @Getter
 public class SessionUser implements Serializable {
     private String name;
+    private String phoneNumber;
     private String email;
+    private Integer state;
+    private String address;
+    private Integer warning;
 
     public SessionUser(User user) {
         this.name = user.getName();
+        this.phoneNumber = user.getPhoneNumber();
         this.email = user.getEmail();
+        this.state = user.getState();
+        this.address = user.getAddress();
+        this.warning = user.getWarning();
     }
 }
