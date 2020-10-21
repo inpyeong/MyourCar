bafd4275eaee9687914824274e970a62379280ba
inpyeong <jinipyung@gmail.com>
2020-10-21 16:33:26 +0900

    Not implemented Car update api yet

--- backend_springboot/src/main/java/com/MyourCar/service/cars/CarsService.java
+++ backend_springboot/src/main/java/com/MyourCar/service/cars/CarsService.java
@@ -1,17 +1,25 @@
 package com.MyourCar.service.cars;
 
+import com.MyourCar.config.auth.dto.SessionUser;
 import com.MyourCar.domain.cars.CarsRepository;
+import com.MyourCar.domain.user.User;
+import com.MyourCar.domain.user.UserRepository;
 import com.MyourCar.web.dto.CarsResponseDto;
 import com.MyourCar.web.dto.CarsSaveRequestDto;
+import com.MyourCar.web.dto.CarsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CarsService {
+    private final UserRepository userRepository;
     private final CarsRepository carsRepository;
+    private final HttpSession httpSession;
 
 //    public CarsResponseDto findById(Long id) {
 //        Cars entity = carsRepository.findById(id)
@@ -21,8 +29,17 @@ public class CarsService {
 //    }
 
     @Transactional
-    public Long savePost(CarsSaveRequestDto carsSaveRequestDto) {
+    public Long save(CarsSaveRequestDto carsSaveRequestDto) {
         return carsRepository.save(carsSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long updateServiceEnable(CarsUpdateRequestDto requestDto) {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        Long id = userRepository.findByEmail(user.getEmail()).get().getId();
+
+        return 1L;
     }
 
 }
--- backend_springboot/src/main/java/com/MyourCar/web/CarsApiController.java
+++ backend_springboot/src/main/java/com/MyourCar/web/CarsApiController.java
@@ -3,6 +3,7 @@ package com.MyourCar.web;
 import com.MyourCar.service.cars.CarsService;
 import com.MyourCar.web.dto.CarsResponseDto;
 import com.MyourCar.web.dto.CarsSaveRequestDto;
+import com.MyourCar.web.dto.CarsUpdateRequestDto;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,11 @@ public class CarsApiController {
 
     @PostMapping("/api/cars")
     public Long create(@RequestBody CarsSaveRequestDto carsSaveRequestDto){
-        return carsService.savePost(carsSaveRequestDto);
+        return carsService.save(carsSaveRequestDto);
+    }
+
+    @PutMapping("/api/cars")
+    public Long update(@RequestBody CarsUpdateRequestDto requestDto) {
+        return carsService.updateServiceEnable(requestDto);
     }
 }
