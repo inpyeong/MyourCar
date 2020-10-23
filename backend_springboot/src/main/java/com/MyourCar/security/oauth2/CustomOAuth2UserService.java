7ff268c3e69195884e35dda936e55e4443b930d1
inpyeong <jinipyung@gmail.com>
2020-10-24 01:12:31 +0900

    Add setter for notnull columns in user table

--- backend_springboot/src/main/java/com/MyourCar/security/oauth2/CustomOAuth2UserService.java
+++ backend_springboot/src/main/java/com/MyourCar/security/oauth2/CustomOAuth2UserService.java
@@ -70,6 +70,9 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         user.setName(oAuth2UserInfo.getName());
         user.setEmail(oAuth2UserInfo.getEmail());
         user.setImageUrl(oAuth2UserInfo.getImageUrl());
+        user.setPhoneNumber("01000000000");
+        user.setState(0);
+        user.setWarning(0);
         return userRepository.save(user);
     }
 
