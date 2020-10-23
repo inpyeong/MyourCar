f399e00a1e0d46885322d0e46c1c2ba9d065c04c
inpyeong <jinipyung@gmail.com>
2020-10-23 21:28:12 +0900

    Add and Modify contollers, entity, dto, service, repository

--- backend_springboot/src/main/java/com/MyourCar/web/UserApiController.java
+++ backend_springboot/src/main/java/com/MyourCar/web/UserApiController.java
@@ -1,8 +1,15 @@
 package com.MyourCar.web;
 
+import com.MyourCar.domain.user.User;
+import com.MyourCar.domain.user.UserRepository;
+import com.MyourCar.exception.ResourceNotFoundException;
+import com.MyourCar.security.CurrentUser;
+import com.MyourCar.security.UserPrincipal;
 import com.MyourCar.service.user.UserService;
 import com.MyourCar.web.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -13,13 +20,18 @@ public class UserApiController {
 
     private final UserService userService;
 
-    @GetMapping("/api/user")
-    public UserResponseDto findByEmail() {
-        return userService.findByEmail();
-    }
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("/api/user/{id}")
-    public UserResponseDto findById(@PathVariable Long id) {
-        return userService.findById(id);
+//    @GetMapping("/api/user/me")
+//    public UserResponseDto findById(@PathVariable Long id) {
+//        return userService.findById(id);
+//    }
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
     }
 }
