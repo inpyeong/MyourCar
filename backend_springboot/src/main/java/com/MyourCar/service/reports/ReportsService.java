c77dab3d41a3c4c289a90b38bc51d550ee157e55
inpyeong <jinipyung@gmail.com>
2020-10-27 01:53:11 +0900

    Remove unnecessary comments

--- backend_springboot/src/main/java/com/MyourCar/service/reports/ReportsService.java
+++ backend_springboot/src/main/java/com/MyourCar/service/reports/ReportsService.java
@@ -1,19 +1,13 @@
 package com.MyourCar.service.reports;
 
-import com.MyourCar.domain.cars.Cars;
-import com.MyourCar.domain.cars.CarsRepository;
 import com.MyourCar.domain.reports.Reports;
 import com.MyourCar.domain.reports.ReportsRepository;
 import com.MyourCar.domain.services.Services;
 import com.MyourCar.domain.services.ServicesRepository;
-import com.MyourCar.domain.user.User;
-import com.MyourCar.domain.user.UserRepository;
 import com.MyourCar.web.dto.ReportsSaveRequestDto;
-import com.MyourCar.web.dto.ServicesSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 
 @Service
@@ -21,14 +15,6 @@ import javax.transaction.Transactional;
 public class ReportsService {
     private final ServicesRepository servicesRepository;
     private final ReportsRepository reportsRepository;
-    private final HttpSession httpSession;
-
-//    public CarsResponseDto findById(Long id) {
-//        Cars entity = carsRepository.findById(id)
-//                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다. id=" + id));
-//
-//        return new CarsResponseDto(entity);
-//    }
 
     @Transactional
     public Long save(ReportsSaveRequestDto reportsSaveRequestDto) {
@@ -37,24 +23,4 @@ public class ReportsService {
         reports.setServices(services);
         return reportsRepository.save(reports).getId();
     }
-
-//    @Transactional
-//    public int patch(long id, CarsUpdateRequestDto carsUpdateRequestDto) {
-//        Optional<Cars> optionalCars = carsRepository.findById(id);
-//        if (optionalCars.isPresent()) {
-//            Cars cars = optionalCars.get();
-//            if (StringUtils.isNotBlank(carsUpdateRequestDto.getReturn_location()))
-//                cars.setReturn_location(carsUpdateRequestDto.getReturn_location());
-//            if (carsUpdateRequestDto.getAvailable_start_time() != null)
-//                cars.setAvailable_start_time(carsUpdateRequestDto.getAvailable_start_time());
-//            if (carsUpdateRequestDto.getAvailable_end_time() != null)
-//                cars.setAvailable_end_time(carsUpdateRequestDto.getAvailable_end_time());
-//            if (carsUpdateRequestDto.getRent_fee() != null)
-//                cars.setRent_fee(carsUpdateRequestDto.getRent_fee());
-//            if (carsUpdateRequestDto.getDriving_fee() != null)
-//                cars.setDriving_fee(carsUpdateRequestDto.getDriving_fee());
-//            return 1;
-//        }
-//        return 0;
-//    }
 }
--- backend_springboot/src/main/java/com/MyourCar/web/ReportsApiController.java
+++ backend_springboot/src/main/java/com/MyourCar/web/ReportsApiController.java
@@ -4,40 +4,19 @@ import com.MyourCar.service.reports.ReportsService;
 import com.MyourCar.service.services.ServicesService;
 import com.MyourCar.web.dto.ReportsSaveRequestDto;
 import com.MyourCar.web.dto.ServicesSaveRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class ReportsApiController {
     private final ReportsService reportsService;
-
-    public ReportsApiController(ReportsService reportsService){
-        this.reportsService = reportsService;
-    }
 
     @PostMapping("/api/reports")
     public Long create(@RequestBody ReportsSaveRequestDto reportsSaveRequestDto){
         return reportsService.save(reportsSaveRequestDto);
     }
 
-
-//    @PutMapping("/api/reviews")
-//    public Long update(@RequestBody CarsUpdateRequestDto requestDto) {
-//        return reviewsService.updateServiceEnable(requestDto);
-//    }
-
-//    @PatchMapping("/api/reviews/{id}")
-//    public Map<String, Object> patch(@PathVariable("id") long id, @RequestBody CarsUpdateRequestDto carsUpdateRequestDto) {
-//        Map<String, Object> response = new HashMap<>();
-//
-//        if(carsService.patch(id, carsUpdateRequestDto) > 0) {
-//            response.put("result", "SUCCESS");
-//        } else {
-//            response.put("result", "FAIL");
-//            response.put("reason", "일치하는 회원 정보가 없습니다. 사용자 id를 확인해주세요.");
-//        }
-//
-//        return response;
-//    }
 }
