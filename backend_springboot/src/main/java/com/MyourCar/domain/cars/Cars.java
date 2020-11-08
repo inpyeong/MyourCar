13316af0aa572b2a1a63a7be8bf5e03d1c21565e
inpyeong <jinipyung@gmail.com>
2020-11-09 02:33:05 +0900

    Add latitude and longitude column

--- backend_springboot/src/main/java/com/MyourCar/domain/cars/Cars.java
+++ backend_springboot/src/main/java/com/MyourCar/domain/cars/Cars.java
@@ -3,14 +3,12 @@ package com.MyourCar.domain.cars;
 import com.MyourCar.domain.reviews.Reviews;
 import com.MyourCar.domain.services.Services;
 import com.MyourCar.domain.user.User;
-import com.MyourCar.web.dto.ReviewsSaveRequestDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -28,59 +26,44 @@ public class Cars {
     private String name;
 
     @Column(nullable = false)
-    private Integer service_enable;
+    private Integer serviceEnable;
 
-    @Column(length = 100, nullable = false)
-    private String return_location;
+    @Column
+    private Double returnLocationLatitude;
 
-    @Column(length = 100, nullable = false)
-    private String current_detailed_location;
+    @Column
+    private Double returnLocationLongitude;
 
-    @Column(length = 100, nullable = false)
-    private String current_district_location;
+    @Column
+    private Double currentLocationLatitude;
+
+    @Column
+    private Double currentLocationLongitude;
 
     @Column
     @Temporal(TemporalType.TIMESTAMP)
-    private Date available_start_time;
+    private Date availableStartTime;
 
     @Column
     @Temporal(TemporalType.TIMESTAMP)
-    private Date available_end_time;
+    private Date availableEndTime;
 
-    @Column(nullable = false)
-    private Integer rent_fee;
+    @Column
+    private Integer rentFee;
 
-    @Column(length = 100, nullable = false)
-    private Integer driving_fee;
+    @Column(length = 100)
+    private Integer drivingFee;
 
-    @Column(length = 100, nullable = false)
+    @Column(length = 100)
     private Integer battery;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
-    @OneToMany(mappedBy = "cars")
+    @OneToMany(mappedBy = "cars", fetch = FetchType.EAGER)
     private Set<Reviews> reviews = new HashSet<>();
-
-//    @OneToOne(mappedBy = "cars")
-//    private Services services;
 
     @OneToMany(mappedBy = "cars")
     private Set<Services> services = new HashSet<>();
 
-    @Builder
-    public Cars(String name, Integer service_enable, String return_location, String current_detailed_location,
-                String current_district_location, Date available_start_time, Date available_end_time, Integer rent_fee,
-                Integer driving_fee, Integer battery) {
-        this.name = name;
-        this.service_enable = service_enable;
-        this.return_location = return_location;
-        this.current_detailed_location = current_detailed_location;
-        this.current_district_location = current_district_location;
-        this.available_start_time = available_start_time;
-        this.available_end_time = available_end_time;
-        this.rent_fee = rent_fee;
-        this.driving_fee = driving_fee;
-        this.battery = battery;
-    }
 }
