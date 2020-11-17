07f74061507e8758d17e85a13b8047563451fac9
inpyeong <jinipyung@gmail.com>
2020-11-17 13:54:25 +0900

    Change timezone and add JpaAuditing

--- backend_springboot/src/main/java/com/MyourCar/Application.java
+++ backend_springboot/src/main/java/com/MyourCar/Application.java
@@ -4,10 +4,20 @@ import com.MyourCar.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
 public class Application {
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
