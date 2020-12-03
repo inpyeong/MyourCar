715d321913c34b2bc6a24d945f4b0555e3d39cce
inpyeong <jinipyung@gmail.com>
2020-12-03 23:20:51 +0900

    Add query methods

--- backend_springboot/src/main/java/com/MyourCar/domain/services/ServicesRepository.java
+++ backend_springboot/src/main/java/com/MyourCar/domain/services/ServicesRepository.java
@@ -4,6 +4,7 @@ import com.MyourCar.domain.cars.Cars;
 import com.MyourCar.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ServicesRepository extends JpaRepository<Services, Long> {
@@ -11,4 +12,8 @@ public interface ServicesRepository extends JpaRepository<Services, Long> {
     Optional<Services> findFirstByUserOrderByCreatedDateDesc(User user);
 
     Optional<Services> findFirstByCarsOrderByCreatedDateDesc(Cars cars);
+
+    List<Services> findAllByCarsIn(List<Cars> carsList);
+
+    Optional<Services> findByUser(User user);
 }
